46c56ebb1f9f28413a6d980ec0a170c588fb1c51
Yang1aa <2316239369@qq.com>
2022-07-23 23:57:32 +0800

    feat：RTKQ第二个参数默认值

--- React/react-advanced/15_reactRedux_RTKQ/src/App.js
+++ React/react-advanced/15_reactRedux_RTKQ/src/App.js
@@ -12,11 +12,11 @@ export default function App() {
         //     }
         //     return result;
         // }, // 用来指定useQuery返回的结果
-        // pollingInterval:500 // 设置轮询的间隔，单位毫秒 如果为0则表示不轮询
+        // pollingInterval:500 // 设置轮询的间隔，单位毫秒 如果为0则表示不轮询 默认0
         // skip:true, // 设置是否跳过当前请求，默认false
-        // refetchOnMountOrArgChange: false, // 设置是否每次都重新加载数据 false正常使用缓存，true每次都重载数据  数字，数据缓存的时间（秒）
-        refetchOnFocus: true, // 是否在重新获取焦点时重载数据
-        refetchOnReconnect: true, // 是否在重新连接后重载数据
+        // refetchOnMountOrArgChange: false, // 设置是否每次都重新加载数据 false正常使用缓存，true每次都重载数据  数字，数据缓存的时间（秒）默认false
+        refetchOnFocus: false, // 是否在重新获取焦点时重载数据 默认false
+        refetchOnReconnect: true, // 是否在重新连接后重载数据 默认true
     });
     return (
         <>
