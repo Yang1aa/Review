20bb54062a525edffcf912588d37772cbee0f30a
Yang1aa <2316239369@qq.com>
2022-07-26 14:09:56 +0800

    feat：props.history参数

--- React/react-advanced/16_reactRouter/reactRouter@5/react-router/src/App.js
+++ React/react-advanced/16_reactRouter/reactRouter@5/react-router/src/App.js
@@ -3,12 +3,14 @@ import { Route } from 'react-router-dom';
 import LinkMenu from './Components/LinkMenu';
 import Home from './Components/Home';
 import About from './Components/About';
+import Student from './Components/Student';
 function App() {
   return (
     <div className="App">
       <LinkMenu />
       <Route exact path='/' component={Home} />
       <Route exact path='/about' component={About} />
+      <Route exact path='/student/:id' component={Student} />
     </div>
   );
 }
