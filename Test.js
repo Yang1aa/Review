700746fa341a03b9da847a00a8d515b6ed6ca20d
Yang1aa <2316239369@qq.com>
2022-08-09 21:15:38 +0800

    feat：实现图片懒加载

--- Test.js
+++ Test.js
@@ -1,59 +1,11 @@
-// 遍历给定节点id的所有子节点，如有结果以数组形式输出
-const tree = [
-    {
-      id: "1",
-      children: [
-        {
-          id: "2",
-          children: [
-            {
-              id: "3",
-              children: [{ id: "4" }]
-            },
-            { id: "5" },
-            {
-              id: "6",
-              children: [{ id: "7" }]
-            },
-          ]
-        },
-        {
-          id: "8",
-          children: [{ id: "9" }]
-        }
-      ]
-    }
-  ]
-  const p = (tree) => {
-    console.log(tree)
-    tree.map(item => {
-      console.log(item)
-      if (item.children) {
-        p(item.children);
-      }
-      arr.push(item.id);
-    })
-  }
-  let arr = [];
-  function fn(tree, num, key) {
-    console.log(tree)
-  
-    tree.forEach(item => {
-      console.log(item[key])
-      if (item[key] === num) {
-      console.log(item.children)
-  
-        if (item.children) {
-          p(item.children)
-        }
-        else {
-          arr.push("当前节点下无子节点");
-        }
-      }
-      if (item.children) {
-        fn(item.children, num, key)
-      }
-    })
-  }
-  fn(tree, '1', 'id')
-  console.log(arr)
+function myNew(construct, ...arg) {
+  let obj = {}
+  obj.__proto__ = construct.prototype;
+  let res = construct.call(obj, arg);
+  return res && typeof (res) === 'object' ? res : obj;
+}
+function Person(name) {
+  this.name = name
+}
+let p1 = myNew(Person, '小明')
+console.log(p1)
